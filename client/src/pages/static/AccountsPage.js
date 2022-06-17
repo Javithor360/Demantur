dd81935e03125014b6f642fb3c902772ad07591d
Javithor360 <floresmejia004@gmail.com>
2022-06-17 17:48:24 -0600

    Diseño de página de tipos de cuenta - Aún queda pendiente su diseño responsive - Corrección de tamaño del hero de la páÂgina de tarjetas - Se agregaron las respectivas imágenes a la página de tipos de cuenta - Asún queda pendiente crear la página individual de cada tipo de cuenta

--- client/src/pages/static/AccountsPage.js
+++ client/src/pages/static/AccountsPage.js
@@ -6,8 +6,20 @@ import { Navbar, Footer } from "../../components/";
 import { Link } from "react-router-dom";
 
 // Assets
-import { BsPiggyBank } from "react-icons/bs";
-import { FaAngleRight } from "react-icons/fa";
+import {
+  BsPiggyBank,
+  BsArrowUpRightCircleFill,
+  BsCreditCard2Back,
+} from "react-icons/bs";
+import {
+  FaAngleRight,
+  FaRegCreditCard,
+  FaMoneyCheckAlt,
+  FaScroll,
+  FaClock,
+} from "react-icons/fa";
+
+const AccountsImg = require.context("./assets/img/acc", true);
 
 export const AccountsPage = () => {
   return (
@@ -20,49 +32,71 @@ export const AccountsPage = () => {
           <p>Conoce nuestra gama de posibilidades</p>
         </div>
       </section>
-      <div className="product-nav-container">
-        <ul className="product-nav-list">
-          <Link to="#" className="product-nav-element">
-            Corriente
-          </Link>
-          <Link to="#" className="product-nav-element">
-            Ahorros
-          </Link>
-        </ul>
-      </div>
-      <section className="acc_main-cards-container">
-        <h2 className="post-nav-title">
-          Echa un vistazo a cada tipo de cuenta
-        </h2>
-        <div className="cards-container">
-          <div className="card">
-            <h2>Cuenta Corriente</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              malesuada orci dui, ac maximus ante laoreet id. Aliquam interdum
-              nibh mauris, eu euismod sem scelerisque vitae. Duis pellentesque
-              urna vitae lectus sodales dapibus. Proin eu odio non lacus
-              tincidunt ultricies. Maecenas sagittis sodales arcu.
+
+      <section className="post-hero-container">
+        <div className="post-hero-info">
+          <p>Tipos de cuenta</p>
+          <hr />
+        </div>
+
+        <div className="acc-container">
+          <div className="acc-details">
+            <h1>Cuenta Corriente</h1>
+            <p className="acc-desc">
+              Disfruta de la liquidez inmediata con tu tarjeta de débito o
+              chequera.
             </p>
-            <Link to="/accounts/checking">
-              <span className="view-text">Ver más</span>
-              <FaAngleRight />
-            </Link>
+            <p className="acc-feature">
+              <FaMoneyCheckAlt color="#455FB9" /> Solicita tu chequera en Línea
+            </p>
+            <p className="acc-feature">
+              <FaScroll color="#455FB9" /> Estado de cuenta digital
+            </p>
+            <p className="acc-feature">
+              <FaRegCreditCard color="#455FB9" /> Tarjeta de débito
+              internacional
+            </p>
+            <div className="acc-btn">
+              <Link to="/accounts/checking">
+                <span>Más información</span>
+                <FaAngleRight color="#fff" />
+              </Link>
+            </div>
           </div>
 
-          <div className="card">
-            <h2>Cuenta de Ahorros</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              malesuada orci dui, ac maximus ante laoreet id. Aliquam interdum
-              nibh mauris, eu euismod sem scelerisque vitae. Duis pellentesque
-              urna vitae lectus sodales dapibus. Proin eu odio non lacus
-              tincidunt ultricies. Maecenas sagittis sodales arcu.
+          <div className="acc-img">
+            <img src={AccountsImg("./acc_checking_card.jpg")} alt="" />
+          </div>
+        </div>
+
+        <div className="acc-container">
+          <div className="acc-details">
+            <h1>Cuenta de Ahorros</h1>
+            <p className="acc-desc">
+              Dispone de tu dinero sin restricción de fondos las 24 horas del
+              día.
             </p>
-            <Link to="/accounts/savings">
-              <span className="view-text">Ver más</span>
-              <FaAngleRight />
-            </Link>
+            <p className="acc-feature">
+              <FaClock color="#455FB9" /> Disponibilidad inmediata
+            </p>
+            <p className="acc-feature">
+              <BsCreditCard2Back color="#455FB9" /> Tarjeta de débito Demantur
+              Classic
+            </p>
+            <p className="acc-feature">
+              <BsArrowUpRightCircleFill color="#455FB9" /> Controla tus
+              movimientos 24/7
+            </p>
+            <div className="acc-btn">
+              <Link to="/accounts/savings">
+                <span>Más información</span>
+                <FaAngleRight color="#fff" />
+              </Link>
+            </div>
+          </div>
+
+          <div className="acc-img">
+            <img src={AccountsImg("./acc_savings_card.jpg")} alt="" />
           </div>
         </div>
       </section>

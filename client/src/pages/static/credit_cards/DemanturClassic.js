6f8fca39cad687249399e2bfe330c722b690f179
DanielVasquezV <86084866+DanielVasquezV@users.noreply.github.com>
2022-06-20 23:56:56 -0600

    Avances en el formulario de solicitud de tarjetas

--- client/src/pages/static/credit_cards/DemanturClassic.js
+++ client/src/pages/static/credit_cards/DemanturClassic.js
@@ -1,5 +1,9 @@
 //scss
 import "../assets/scss/credit_cards/demantur_classic_main.scss";
+//icons
+import { FaRegUser, FaRegAddressCard } from "react-icons/fa"
+import { MdOutlineEmail } from "react-icons/md"
+import { BsTelephone, BsHouseDoor } from "react-icons/bs"
 
 //components
 import { Navbar, Footer } from "../../../components";
@@ -95,7 +99,110 @@ export const DemanturClassic = () => {
             <hr className="blue-underline smallest"></hr>
             <p>Solicita tu tarjeta ahora</p>
           </div>
+
           <form className="classic-card-form">
+            <div className="form-column-1">
+
+              <div className="form-column-content-1">
+
+                <div className="form-individual-element">
+                  
+                  <div className="label-icon"><FaRegUser /></div>
+                  <div className="form-input-container">
+                      <input className="form-input-box" type="text" placeholder="Nombre completo"/>
+                      <label className="form-label-box" htmlFor="">
+                          Nombre completo
+                      </label>
+                  </div>
+
+                </div>
+
+                <div className="form-individual-element">
+                  
+                  <div className="label-icon"><BsTelephone /></div>
+                  <div className="form-input-container">
+                      <input className="form-input-box" type="text" placeholder="Número de contacto"/>
+                      <label className="form-label-box" htmlFor="">
+                          Número de contacto
+                      </label>
+                  </div>
+
+                </div>
+
+                <div className="form-individual-element">
+                  
+                  <div className="label-icon"><BsHouseDoor /></div>
+                  <div className="form-input-container">
+                      <input className="form-input-box" type="text" placeholder="Dirección"/>
+                      <label className="form-label-box" htmlFor="">
+                          Dirección
+                      </label>
+                  </div>
+
+                </div>
+
+              </div>
+              
+              <div className="form-column-content-1">
+                <div className="form-individual-element">
+                    
+                    <div className="label-icon"><FaRegAddressCard /></div>
+                    <div className="form-input-container">
+                        <input className="form-input-box" type="number" placeholder="Nombre completo"/>
+                        <label className="form-label-box" htmlFor="">
+                            Número de DUI
+                        </label>
+                    </div>
+
+                </div>
+                <div className="form-individual-element">
+                    
+                    <div className="label-icon"><MdOutlineEmail /></div>
+                    <div className="form-input-container">
+                        <input className="form-input-box" type="number" placeholder="Nombre completo"/>
+                        <label className="form-label-box" htmlFor="">
+                            Correo Electrónico
+                        </label>
+                    </div>
+
+                </div>
+                
+                <div className="individual-element-flexrow">
+                  <select name="" id="">
+                    <option>Rango Salarial</option>
+                    <option>Entre $450 y $499</option>
+                    <option>Entre $500 y $999</option>
+                    <option>Entre $1,000 y $1,999</option>
+                    <option>Entre $2,000 y $3,999</option>
+                    <option>Entre $4,000 y $5,999</option>
+                    <option>Mayor a $6,000</option>
+                  </select>
+                  <div className="form-individual-element">
+                    <div className="form-input-container">
+                        <input className="form-input-box" type="text" placeholder="Escriba su posición laboral"/>
+                        <label className="form-label-box" htmlFor="">
+                            Escriba su posición laboral
+                        </label>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            <div className="form-column-2">
+              <div className="input-files">
+                <p>Fotocopia de DUI (revés y derecho)</p>
+                {/* <input type="file"></input> */}
+              </div>
+              <div className="input-files">
+                <p>Fotocopia de NIT</p>
+                {/* <input type="file"></input> */}
+              </div>
+              <div className="input-files">
+                <p>Adjunta tu constancia de salario</p>
+                {/* <input type="file"></input> */}
+              </div>
+            </div>
           </form>
         </div>
         <Footer />

5e7d12ff78b112bc9b32711fcf46155f7396037b
DanielVasquezV <86084866+DanielVasquezV@users.noreply.github.com>
2022-07-19 23:07:50 -0600

    Cambios esteticos en el Sidebar, además de un pequeño avance con respecto a los estilos globales

--- client/src/pages/private/components/SideBar.js
+++ client/src/pages/private/components/SideBar.js
@@ -1,5 +1,4 @@
 //icons
-
 import { NavLink} from "react-router-dom"
 import { AiFillHome } from 'react-icons/ai'
 import { MdAccountBalanceWallet, MdPermContactCalendar, MdOutlineLogout } from 'react-icons/md'
@@ -13,48 +12,60 @@ const SideBarImages = require.context('./assets/img/', true)
 export const SideBar = () => {
   return (
     <>
-        <div className="h-full w-[20%] p-2 bg-white rounded-md shadow-lg flex flex-column items-center justify-between">
-          <div className="w-full">
-            <div className="side-bar-logo w-full">
+        <div className="side-bar-container h-full w-64 p-2 bg-white rounded-md shadow-lg flex flex-column items-center justify-between">
+          <div className="side-bar-content-container w-full">
+            <div className="side-bar-logo w-full content-column-1">
                 <img className="w-44 mt-4 mx-auto block" src={ SideBarImages('./Demantur_Imagotype-2.png') } alt=""/>
                 <hr className="black-line"/>
             </div>
-            <div className="menu-items w-full">
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
-                <div className="menu-icon ml-5 mr-4">
-                  <AiFillHome/>
+            <div className="menu-items w-full content-column-2">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+                <div className="menu-icon ml-5 mr-4 h-full">
+                  <AiFillHome className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Home Page
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Home Page
+                </span>
               </NavLink>
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
                 <div className="menu-icon ml-5 mr-4">
-                  <MdAccountBalanceWallet/>
+                  <MdAccountBalanceWallet className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Cuentas
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Cuentas
+                </span>
               </NavLink>
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
                 <div className="menu-icon ml-5 mr-4">
-                  <BiTransfer/>
+                  <BiTransfer className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Transferencias
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Transferecias
+                </span>
               </NavLink>
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
                 <div className="menu-icon ml-5 mr-4">
-                  <FaListAlt/>
+                  <FaListAlt className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Prestamos
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Préstamos
+                </span>
               </NavLink>
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
                 <div className="menu-icon ml-5 mr-4">
-                  <MdPermContactCalendar/>
+                  <MdPermContactCalendar className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Contactos
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Contactos
+                </span>
               </NavLink>
-              <NavLink to="/dashboard" className="menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
+              <NavLink to="/dashboard" className="group menu-item mb-2 p-1 flex flex-row w-full rounded items-center">
                 <div className="menu-icon ml-5 mr-4">
-                  <BsCreditCardFill/>
+                  <BsCreditCardFill className="group-hover:text-white group-hover:bg-[#323643] ease-in-out duration-200 p-1 rounded"/>
                 </div>
-                Tarjetas
+                <span className="group-hover:text-[#323643] ease-in-out duration-200 mt-2">
+                  Tarjetas
+                </span>
               </NavLink>
             </div>
           </div>

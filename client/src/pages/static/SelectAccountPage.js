32a214e984c483ccac1c8eafe982df9f4d84ad6f
Akilelk23 <luisernestomr1503@gmail.com>
2022-07-26 22:06:13 -0600

    Cambios minimos
    inicio de cambio de página de selección de cuenta y traducción en español de InfoEmpre anteriormente no se subió el comentario

--- client/src/pages/static/SelectAccountPage.js
+++ client/src/pages/static/SelectAccountPage.js
@@ -8,6 +8,7 @@ import { Navbar, Footer } from "../../components";
 import { BsFillCursorFill as Select } from "react-icons/bs";
 import { FaUserTie as Empre } from "react-icons/fa";
 import { FaUserAlt as User } from "react-icons/fa";
+const TarjetasImages = require.context("./assets/img/index", true);
 /* import { Link } from "react-router-dom";
 const CuentasImagenes = require.context('./assets/img/banner', true); */
 
@@ -31,44 +32,41 @@ export const SelectAccountPage = () => {
         tipo de Usuario al cual eres poseedor, asegurate de seleccionar la
         opcion con tu tipo de cuenta e todos tus datos correctamente
       </p>
-      <div className="boxs-wrap">
-        <div className="boxs">
-          <br />
-          <div className="title2">
-            <Empre className="iconst" />
-            <br />
-            <p className="subtitle">Cuenta Empresarial</p>
-            <hr />
-          </div>
-          <br />
-          <p className="info2">
-            si tu empresa aún no tiene una cuenta con nosotros, cotratala de
-            manera segura, ágil y digital sin acudir a una oficina o un banco.
-          </p>
-          <div className="boton-position">
-            <button class="custom-btn btn-15">Iniciar sesion</button>
-            <button class="custom-btn btn-15">Registrarce</button>
-          </div>
-        </div>
-        <div className="boxs">
-          <br />
-          <div className="title2">
-            <User className="iconst" />
-            <br />
+      <div className="cards-container">
+          <div className="card">
+            <div className="title2">  
+            <img src={TarjetasImages("./card-2.png")} alt="" />
             <p className="subtitle">Cuenta Normal</p>
             <hr />
           </div>
-          <br />
-          <p className="info2">
-            La mejor opcion en cuanto a administracion de tu estado bancario,
-            todo desde la comodidad de tu hogar sin tener que ira un banco
+            <p>si tu empresa aún no tiene una cuenta con nosotros, cotratala de
+            manera segura, ágil y digital sin acudir a una oficina o un banco.
           </p>
-          <div className="boton-position">
+            <div className="boton-position">
             <button class="custom-btn btn-15">Iniciar sesion</button>
             <button class="custom-btn btn-15">Registrarce</button>
           </div>
+            <div className="boton">
+             
+            </div>
+          </div>
+          <div className="card">
+            <div className="title2">
+            <img src={TarjetasImages("./card-3.png")} alt="" />
+            <p className="subtitle">Cuenta Empresarial</p>
+            <hr />
+          </div>
+            <p>La mejor opcion en cuanto a administracion de tu estado bancario,
+            todo desde la comodidad de tu hogar sin tener que ira un banco</p>
+            <div className="boton-position">
+            <button class="custom-btn btn-15">Iniciar sesion</button>
+            <button class="custom-btn btn-15">Registrarce</button>
+          </div>
+            <div className="boton">
+              
+            </div>
+          </div>
         </div>
-      </div>
 
       <Footer />
     </>

5530077efa671c1127973325494581109806963b
Akilelk23 <luisernestomr1503@gmail.com>
2022-08-03 15:31:48 -0600

    cambios mínimos
    diseño correo verificación

--- server/helpers/Functions.js
+++ server/helpers/Functions.js
@@ -156,8 +156,117 @@ exports.createCode = () => {
 exports.VeCoEmail = async (verifyCode, isNormalUser, next) => {
   try {
     const message = `
-    <h1>CODIGO DE VERIFICACION:</h1>
-    <p><strong>${verifyCode}</strong></p>
+    <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <style>
+        body{
+            font-family: 'Poppins', sans-serif;
+        }
+        .fondo{
+            margin: 0 auto;
+            height: 57rem;
+            width: 60rem;
+            border: solid;
+            border-color: rgba(0, 0, 0, 0.068);
+        }
+        .icon{
+            width: 15rem;
+            margin-top: 2rem;
+            margin-left: 2rem;
+            margin-bottom: 2rem;
+        }
+    .baners {
+    background-image: url(https://cdn.discordapp.com/attachments/861438024659501087/1004499581272334356/personas-que-esperan-obtener-dinero-cajero-automatico-personas-retiran-dinero-concepto-cajero-automatico_1150-13566.webp);
+    background-size: cover;
+    width: 100%;
+    height: 22rem;
+    position: relative;
+
+}
+
+.baners-content {
+    position: absolute;
+    width: inherit;
+    height: inherit;
+    background-color: rgba(0, 0, 0, 0.342);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #ffffff;
+}
+.subtitles{
+    text-align: center;
+    margin: 1rem auto 0 auto;
+    font-size: 1.8rem;
+}
+.title{
+    font-size: 3rem;
+            font-weight: bold;
+            margin: 1.25rem auto 1.25rem auto ;
+}
+.title2 hr{
+    border: 0;
+    height: .3rem;
+    background: #93DEFF;
+    opacity: 100;
+    padding: 0;
+    margin: auto;
+    width: 15rem;
+    font-weight: bold;
+}
+
+.title2 p{
+    text-align: center;
+    margin: 2rem auto 0 auto;
+    font-size: 2.5rem;
+}
+.subs{
+    text-align: center;
+    font-size: 2rem;
+    margin-left: 5rem;
+    margin-right: 5rem;
+}
+.copyright{
+    font-size: 1.5rem;
+  padding: 0.625rem 0;
+    color: #ffffff;
+}
+.a{
+    text-align: center;
+    font-size: 2rem;
+}
+footer{
+    background-color: rgb(78, 78, 78);
+    margin-top: 14rem;
+    height: 3rem;
+    text-align: center;
+}
+    </style>
+  </head>
+  <body>
+      
+      <div class="fondo"><img class="icon" src="https://media.discordapp.net/attachments/825146450232213505/999324633478205560/Demantur_Imagotype-3.png?width=1025&height=414" alt="">
+          <div class="baners">
+              <div class="baners-content">
+                <h1 class="title">Demantur Banck</h1>
+              </div>
+            </div>
+            <div class="title2">
+              <p>CODIGO DE VERIFICACION:</p>
+              <hr />
+            </div>
+    <p class="subtitles"><strong>${verifyCode}</strong></p>
+    
+            <footer>
+              <span class="copyright">
+                  Todos los derechos reservados. © 2022 Demantur
+              </span>
+            </footer>
+      </div>
+      
+  </body>
     `;
 
     await sendEmail({

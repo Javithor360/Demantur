4ebde6fc33c9576e5130f163c36f813296c15fc0
MatMT <86084866+DanielVasquezV@users.noreply.github.com>
2022-08-18 15:19:30 -0600

    Cambios Dashboard del empleado

--- client/src/pages/private/DashboarEmple.js
+++ client/src/pages/private/DashboarEmple.js
@@ -1,12 +1,49 @@
-import NavBar from "../private/components/DashboardEmple/SideBar.js";
+import { useState } from "react";
+import { SideBar } from "./components/DashboardEmple/SideBarEmployee";
+import { Header } from "./components/DashboardEmple/HeaderEmployee";
 import "./assets/scss/dashboarde.scss";
-
+import { useDash } from "../../context/DashboardContext";
+import { HomePageEmployee, LoansRequests, CardsRequests, Requests } from "./components/DashboardEmple/DashEmployeeElements/IndexEmployeeDash";
 
 export const DashboarEmple = () => {
+  const { Option, SettingsOption, GeneralInfoQuery } = useDash();
+
+  const DisplayElementEmployee = () => {
+    switch (Option) {
+      case 1:
+        return <HomePageEmployee />;
+      case 2:
+        return <LoansRequests />;
+      case 3:
+        return <CardsRequests />;
+      case 4:
+        return <Requests />
+      default:
+        return <h1>Home Page</h1>;
+    }
+  };
   return (
     <>
-     <header className="fondo"></header>
-     <NavBar />
+     
+      <div className="w-screen h-screen bg-[#F1F1F1] relative">
+        <div className="w-full h-2/5 bg-[#396EB0] absolute fondo"></div>
+        <div className="absolute flex items-center justify-center w-full h-full">
+          <div className="w-[98%] h-[95%] flex">
+            <SideBar />
+            <div className="h-[95%] w-[80%]  mx-auto">
+              <Header />
+              <div className="pl-4 Display-dash-div">
+                <div className="h-100">
+                  <div className="flex flex-col justify-between py-3 w-100 h-100">
+                    {DisplayElementEmployee()}
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ client/src/pages/private/components/DashboardEmple/DashEmployeeElements/Requests.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Requests = () => {
+  return (
+    <div className='h-full w-full bg-white rounded-xl p-3'>
+        <p>Solicitudes</p>
+    </div>
+  )
+}

d3cc96ad307e755384715d9352f9c626f9effd97
✯ᗪᗴᗩᗪ ᑭOOᒪ༆♤ <87096532+Deadpool345@users.noreply.github.com>
2022-08-19 22:36:16 -0600

    avances en el dashboard del empleado

--- client/src/pages/private/components/DashboardEmple/DashEmployeeElements/Requests.js
+++ client/src/pages/private/components/DashboardEmple/DashEmployeeElements/Requests.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../assets/scss/RequestEmployee.scss'
+
 
 export const Requests = () => {
   return (
-    <div className='h-full w-full bg-white rounded-xl p-3'>
-        <p>Solicitudes</p>
+    <div className='request-employee-container '>
+        <div className='request-container'>
+          <div className='tittle-request'>
+            <p>Solicitudes pendientes:</p>
+          </div>
+          <div className='request-card'>
+            <p>Solicitudes n°120</p>
+            <button className='button-request'>Más Info</button>
+          </div>
+          <div className='request-card'>
+            <p>Solicitudes n°121</p>
+            <button className='button-request'>Más Info</button>
+          </div>
+          <div className='request-card'>
+            <p>Solicitudes n°122</p>
+            <button className='button-request'>Más Info</button>
+          </div>
+        </div>
     </div>
   )
 }

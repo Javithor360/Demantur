6ba4a54f4ea64b99b3ed16ff306cb6a08690efb1
✯ᗪᗴᗩᗪ ᑭOOᒪ༆♤ <87096532+Deadpool345@users.noreply.github.com>
2022-08-23 23:29:24 -0600

    avances en el modal de depositos, sujeta a cambios

--- client/src/pages/private/components/DashboardEmployeeElements/ConfirmAction.js
+++ client/src/pages/private/components/DashboardEmployeeElements/ConfirmAction.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react'
+import './assets/scss/DepositsEmployee.scss'
 
 export const ConfirmAction = ({ props, setActive, setSuccess }) => {
     const handleButton = async () => {
@@ -23,17 +24,19 @@ export const ConfirmAction = ({ props, setActive, setSuccess }) => {
     }
 
     return (
-        <div className='h-full p-[4rem] w-full'>
-            <div>
+        <div className='h-full  p-[4rem] w-full'>
+            <div className='container-confir_action'>
                 <h1 className='text-center'>Confirma tu acción</h1>
                 <p>Antes de continuar, asegúrate que los datos estén correctos y posteriormente confirma la solicitud con el botón al final.</p>
 
                 <div>
                     <div className='mt-[3rem]'>
                         <h4>Cliente:</h4>
+                        <p>Foto de Perfil:</p> 
                         <div className='flex'>
-                            <img src={props.uPfp} alt="foto de perfil" className='h-[12rem] w-[12rem]' />
-                            <div className='ml-[3rem] flex flex-col justify-center'>
+                            <img src={props.uPfp} alt="" className='h-[12rem] w-[12rem]' />
+                              
+                            <div className='ml-[3rem] flex flex-col justify-center number-account-employee'>
                                 <p>{props.uFullName}</p>
                                 <p>Número de cuenta: {props.uAcc}</p>
                             </div>
@@ -44,7 +47,7 @@ export const ConfirmAction = ({ props, setActive, setSuccess }) => {
                         <div className='basis-[50%]'>
                             <h4>Transacción:</h4>
                             <p><strong>Tipo:</strong> Depósito</p>
-                            <p><strong>Monto:</strong> <span className='text-[2rem]'>${props.amount}</span></p>
+                            <p><strong>Monto:</strong> <span className='text-[1.5   rem]'>${props.amount}</span></p>
                             <p><strong>Fecha:</strong> {new Date().toLocaleDateString()}</p>
                         </div>
                         <div className='basis-[50%]'>
@@ -55,8 +58,8 @@ export const ConfirmAction = ({ props, setActive, setSuccess }) => {
                     </div>
 
                     <div className='flex justify-center'>
-                        <button onClick={handleButton} className="m-[2rem]">CONFIRMAR</button>
-                        <button onClick={closeButton} className='m-[2rem]'>CANCELAR</button>
+                        <button onClick={handleButton} className="m-[2rem] btn-confirm">CONFIRMAR</button>
+                        <button onClick={closeButton} className='m-[2rem] btn-confirm'>CANCELAR</button>
                     </div>
                 </div>
             </div>

523128c9ccea3201fcdfb48787ecf27faf9c9d7b
Javithor360 <floresmejia004@gmail.com>
2022-09-06 07:49:01 -0600

    Arreglo validación mínimo

--- client/src/pages/private/components/DashboardEmployeeElements/Requests.js
+++ client/src/pages/private/components/DashboardEmployeeElements/Requests.js
@@ -19,7 +19,7 @@ export const Requests = () => {
       const res = await getActivatedAccountRequests(localStorage.getItem('employeeToken'));
       setInfo({ MainInfo: res.data.data[0], ExtraInfo: res.data.data[1] });
     })()
-  })
+  }, [])
 
   return (
     <>
@@ -29,48 +29,49 @@ export const Requests = () => {
             <p className="text-gray-500 text-center text-[28px] m-0 mb-4 p-0">Solicitudes de activación de cuenta</p>
             <div className='flex flex-col-reverse justify-between text-center'>
               {
-                Info.length !== 0 ?
+                // Mayor que cero porque al no llevar nada, el array es undefined
+                Info.length > 0 ?
                   Info.MainInfo.map((el, i) => {
                     let Name = `${el.FirstName} ${el.LastName}`
                     return (
                       <>
-                      <div className='flex flex-col w-[100%] h-[12rem] border-cover-2 rounded-lg mb-5'>
-                        <div className='flex flex-row h-[65%] w-full'>
-                          <div className='w-[40%] h-full'>
-                            <div className='bg-[#D6D6D6] h-[25%] table mb-0 rounded-tl-lg'>
-                              <span className='table-cell align-middle'>Nombre</span>
+                        <div className='flex flex-col w-[100%] h-[12rem] border-cover-2 rounded-lg mb-5'>
+                          <div className='flex flex-row h-[65%] w-full'>
+                            <div className='w-[40%] h-full'>
+                              <div className='bg-[#D6D6D6] h-[25%] table mb-0 rounded-tl-lg'>
+                                <span className='table-cell align-middle'>Nombre</span>
+                              </div>
+                              <div className='h-[75%] table mb-0'>
+                                <span className='table-cell align-middle'>{Name}</span>
+                              </div>
                             </div>
-                            <div className='h-[75%] table mb-0'>
-                              <span className='table-cell align-middle'>{Name}</span>
+                            <div className='w-[12%] h-full'>
+                              <div className='bg-[#D6D6D6] h-[25%] table mb-0 border-left-division'>
+                                <span className='table-cell align-middle'>Dui</span>
+                              </div>
+                              <div className='h-[75%] table mb-0 border-left-division'>
+                                <span className='table-cell align-middle'>{el.Dui}</span>
+                              </div>
+                            </div>
+                            <div className='w-[35%] h-full'>
+                              <div className='bg-[#D6D6D6] h-[25%] table mb-0 border-left-division'>
+                                <span className='table-cell align-middle'>Correo electrónico</span>
+                              </div>
+                              <div className='h-[75%] table mb-0 border-left-division'>
+                                <span className='table-cell align-middle'>{el.Email}</span>
+                              </div>
+                            </div>
+                            <div className='w-[13%] h-full'>
+                              <div className='bg-[#D6D6D6] h-[25%] table mb-0 rounded-tr-lg border-left-division'>
+                                <span className='table-cell align-middle'>Creada el:</span>
+                              </div>
+                              <div className='h-[75%] table mb-0 border-left-division'>
+                                <span className='table-cell align-middle'>{new Date(el.createdAt).toLocaleDateString()}</span>
+                              </div>
                             </div>
                           </div>
-                          <div className='w-[12%] h-full'>
-                            <div className='bg-[#D6D6D6] h-[25%] table mb-0 border-left-division'>
-                              <span className='table-cell align-middle'>Dui</span>
-                            </div>
-                            <div className='h-[75%] table mb-0 border-left-division'>
-                              <span className='table-cell align-middle'>{el.Dui}</span>
-                            </div>
-                          </div>
-                          <div className='w-[35%] h-full'>
-                            <div className='bg-[#D6D6D6] h-[25%] table mb-0 border-left-division'>
-                              <span className='table-cell align-middle'>Correo electrónico</span>
-                            </div>
-                            <div className='h-[75%] table mb-0 border-left-division'>
-                              <span className='table-cell align-middle'>{el.Email}</span>
-                            </div>
-                          </div>
-                          <div className='w-[13%] h-full'>
-                            <div className='bg-[#D6D6D6] h-[25%] table mb-0 rounded-tr-lg border-left-division'>
-                              <span className='table-cell align-middle'>Creada el:</span>
-                            </div>
-                            <div className='h-[75%] table mb-0 border-left-division'>
-                              <span className='table-cell align-middle'>{new Date(el.createdAt).toLocaleDateString()}</span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className='h-[35%] w-full flex items-center border-top-division'>
-                          <button className='my-auto mx-auto block outline-none border-none px-2 py-2 rounded bg-[#455FB9] text-white' onClick={(e) => {
+                          <div className='h-[35%] w-full flex items-center border-top-division'>
+                            <button className='my-auto mx-auto block outline-none border-none px-2 py-2 rounded bg-[#455FB9] text-white' onClick={(e) => {
                               e.preventDefault();
                               setDisplayDetails(true);
                               setParams({
@@ -78,8 +79,8 @@ export const Requests = () => {
                                 ExtraInfo: Info.ExtraInfo[i]
                               })
                             }}>Más detalles</button>
+                          </div>
                         </div>
-                      </div>
                       </>
                     )
                   })

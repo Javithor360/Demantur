471ce615bca1e9da9e9dc3cf8634eca99380ffd9
RealAlvinJS <alvinjosuemelendez@gmail.com>
2022-09-07 22:42:59 -0600

    wtf

--- server/routes/dashboard.routes.js
+++ server/routes/dashboard.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthMiddleware = require('../middlewares/AuthMiddleware');
 
 const { testDB, getUserId, getGlobalInfo, getFriendsReq, addFriendRequest, CancelPendingFr, AcceptFriend, DeclineFriend, DeleteFriend, DoAtransfer, getMyCardReq, getMyLoanReq, getContacs, getSavAcc, UploadPhoto, getNavName, getEveryAcc, ChangeEmail, getAccountsHistory, EmailCodeVer, CancelChangeEmail,
-  VerifyOldPass, ChangePass, VerifyCodePass, CancelChangePass, getPedingFriendReq, FriendReq, getUsersToAdd, getMyCard, getDebitCard, PayCardDebt, CreateDebitCard, getMyCredit
+  VerifyOldPass, ChangePass, VerifyCodePass, CancelChangePass, getPedingFriendReq, FriendReq, getUsersToAdd, getMyCard, getDebitCard, PayCardDebt, CreateDebitCard, getMyCredit, PayLoan
 } = require('../controllers/DashboardNUController');
 
 router.route('/test-db-relation').post([AuthMiddleware], testDB);
@@ -41,5 +41,6 @@ router.route('/get-debit-card').get([AuthMiddleware], getDebitCard);
 router.route('/pay-card-debt').post([AuthMiddleware], PayCardDebt);
 router.route('/create-debit-card').post([AuthMiddleware], CreateDebitCard);
 router.route('/get-my-credit').get([AuthMiddleware], getMyCredit)
+router.route('/pay-loan').post([AuthMiddleware], PayLoan)
 
 module.exports = router;
